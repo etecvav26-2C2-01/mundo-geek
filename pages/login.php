d98a025e4c15da9e3d56b1f479c29e592393ea97
brayanfn <brayanekauan1301@gmail.com>
2026-06-12 09:24:27 -0300

    Corrige erros de sintaxe e logica de login

--- pages/login.php
+++ pages/login.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/../lang/' . $lang . '.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/app.php';
 
-if (isset($_SESSION['user'])) {
+if (isset($_SESSION['user_id'])) {
     header('Location: '. BASE_URL .'/pages/index.php');
     exit;
 }
@@ -32,21 +32,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
-        if ($user) {
-            if(password_verify($_POST['password'], $user['password'])){
-                $_SESSION['user_id'] = $user['id'];
-                $_SESSION['user_name'] = $user['username'];
+        if($user && password_verify($password, $user['password'])){
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_name'] = $user['username'];
 
-                header('Location: '. BASE_URL .'/pages/index.php');
-                exit;
-            }else{
-                $errorMessage = 'Incorrect password combination'; 
+            header('Location: '. BASE_URL .'/pages/index.php');
             exit;
-            }
-
         }else{
-            $errorMessage = 'No user named "' .$username. '" found'; 
-            exit;
+            $errorMessage = 'Invalid username or password'; 
         }
     }
 }
@@ -137,3 +130,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+</html>
