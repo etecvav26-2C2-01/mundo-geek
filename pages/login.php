25039ba8079ece1e6fb3234f3d6c4e81571f7ff8
brayanfn <brayanekauan1301@gmail.com>
2026-06-12 08:55:55 -0300

    Correção de erro de sintaxe

--- pages/login.php
+++ pages/login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
         if ($user) {
-            if(password_verify($_POST['senha'], $user['password'])){
+            if(password_verify($_POST['password'], $user['password'])){
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['user_name'] = $user['username'];
 
