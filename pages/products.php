cdc1d41eeed6d514e09d386896f30d668edb5d93
brayanfn <brayanekauan1301@gmail.com>
2026-06-13 13:30:11 -0300

    Adiciona navbar, header e corrige includes

--- pages/products.php
+++ pages/products.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 
-require __DIR__ . '/../config/database.php';
-require __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../includes/navbar.php';
 
 $sql = "SELECT *
         FROM products 
@@ -13,21 +14,6 @@ $stmt->execute();
 
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-<!DOCTYPE html>
-<html lang="<?= htmlspecialchars($lang) ?>">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mundo Geek - Produtos</title>
-
-    <link href="../assets/css/style.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-
-<body>
-    <?php require_once __DIR__ . '../includes/header.php'; ?>
     <main class="container min-vh-100 d-flex align-items-center justify-content-center py-4">
         <section class="row w-100 shadow-lg rounded-4 overflow-hidden">
 
