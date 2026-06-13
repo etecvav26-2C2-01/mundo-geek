a282637a4ea41961e37c8564408274c3727401f3
brayanfn <brayanekauan1301@gmail.com>
2026-06-13 19:03:16 -0300

    Corrige erro de sintaxe e remove html desnecessário

--- pages/login.php
+++ pages/login.php
@@ -41,23 +41,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header('Location: '. BASE_URL .'/pages/index.php');
             exit;
         }else{
-            $errorMessage = $text['Error_Login']; 
+            $errorMessage = $text['error_login']; 
         }
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="<?= htmlspecialchars($lang) ?>">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mundo Geek - Login</title>
-    
-    <link href="../assets/css/style.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-<body>
     <main class="container min-vh-100 d-flex align-items-center justify-content-center py-4">
     <section class="row w-100 shadow-lg rounded-4 overflow-hidden">
 
@@ -70,14 +59,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
             
             <h2 class="h4 mb-3">
-                Login
+                <?= $text['login'] ?>
             </h2>
 
             <form method="post" action="login.php">
 
                 <div class="mb-3">
                     <label for="username" class="form-label">
-                        <?= $text['Username'] ?>
+                        <?= $text['username'] ?>
                     </label>
                     <input 
                         type="text" 
@@ -90,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 <div class="mb-3">
                     <label for="password" class="form-label">
-                        <?= $text['Password'] ?>
+                        <?= $text['password'] ?>
                     </label>
                     <input 
                         type="password" 
@@ -108,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <?php endif; ?>
 
                 <button type="submit" class="btn w-100 mb-3">
-                    Login
+                    <?= $text['login'] ?>
                 </button>
 
             </form>
