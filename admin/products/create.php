7ab9c5368e7d6695780105bd3e4afc43e465f3c5
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-06-14 13:44:46 -0300

    Require_once do footer, navbar, auth, auth-admin e header adicinados

--- admin/products/create.php
+++ admin/products/create.php
@@ -2,6 +2,12 @@
 
 $currentpage = 'products';
 
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../includes/auth.php';
+require_once __DIR__ . '/../../includes/auth-admin.php';
+require_once __DIR__ . '/../../includes/header.php';
+require_once __DIR__ . '/../../includes/navbar.php';
+
 ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
