3c12b299a3ee7e28fa907bacc9aae8351e5a2229
Gustavo de Lima Costa <gustavo.limacosta1804@gmail.com>
2026-06-13 23:50:26 -0300

    Adição do footer.php ao repositório do site

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer text-center py-3 mt-5">
+    <small>
+        Mundo Geek - Cultura Pop e Jogos
+    </small>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
