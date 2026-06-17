a4e47d18735ad8ced569faf1de75c4916139d7c8
brayanfn <brayanekauan1301@gmail.com>
2026-06-17 15:21:02 -0300

    Ajuste da dashboard para incluir requires novamente

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,11 @@
 <?php
+ 
+require_once __DIR__ . '/../config/database.php';   
+require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/auth-admin.php';
 
-require_once __DIR__ . '/../config/init.php';   
+require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../includes/navbar.php';
 
 $sql = "SELECT COUNT(*)
         FROM products ";
