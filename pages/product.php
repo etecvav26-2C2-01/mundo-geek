4e66e3471cf66256cc31cd1622c3c0673ca3d4fc
brayanfn <brayanekauan1301@gmail.com>
2026-06-17 15:43:18 -0300

    Ajuste de sintaxe no product.php

--- pages/product.php
+++ pages/product.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/../config/database.php';
+
 require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/navbar.php';
 
