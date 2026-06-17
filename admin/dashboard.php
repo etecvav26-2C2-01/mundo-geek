5cb9778ccb26b209c33b69c59d12ce4f450d7355
brayanfn <brayanekauan1301@gmail.com>
2026-06-17 15:44:38 -0300

    Adiciona require do app.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,8 @@
 <?php
- 
+
+require_once __DIR__ . '/../config/app.php';
 require_once __DIR__ . '/../config/database.php';   
+
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/auth-admin.php';
 
