7396a3d05b1d00b88e1d5d3a2ed5aebf2bedbe42
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-06-17 15:50:05 -0300

    Adiciona require do app.php

--- admin/products/index.php
+++ admin/products/index.php
@@ -1,8 +1,11 @@
 <?php
 
+require_once __DIR__ . '/../../config/app.php';
 require_once __DIR__ . '/../../config/database.php';
+
 require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../includes/auth-admin.php';
+
 require_once __DIR__ . '/../../includes/header.php';
 require_once __DIR__ . '/../../includes/navbar.php';
 
