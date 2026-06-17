b72a58edc2b177a4f7dae99e081641f66ef626a9
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-06-17 14:49:56 -0300

    Ajuste de Sintaxe

--- config/init.php
+++ config/init.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . 'app.php';
-require_once __DIR__ . 'database.php';
+require_once __DIR__ . '/app.php';
+require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/auth-admin.php';
 require_once __DIR__ . '/../includes/header.php';
