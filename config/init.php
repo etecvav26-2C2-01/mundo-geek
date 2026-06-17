7da85c8356f0b154f4e37c8f0ba6fca8d0283312
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-06-17 14:40:51 -0300

    Adiciona arquivo init.php
    Adiciona arquivo init.php para fins de manutenibilidade e testes

---
+++ config/init.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . 'app.php';
+require_once __DIR__ . 'database.php';
+require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/auth-admin.php';
+require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../includes/navbar.php';
+
+
+?>
