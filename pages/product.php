b8db0c31d94ccaec94a3abaa6dc0fc11a8708d5b
William Lucas da Silva <williamlucasd04@gmail.com>
2026-06-24 11:47:16 -0300

    Ajuste das classes em product.php

--- pages/product.php
+++ pages/product.php
@@ -23,41 +23,44 @@ $product = $stmt->fetch(PDO::FETCH_ASSOC);
 <main class="container min-vh-100 d-flex justify-content-center py-4">
     <section class="row justify-content-center">
         <div class="col-12 w-100">
-            <div class="card-produto card">
+            <div class="card product-detail">
                 <?php if (!empty($product['image'])): ?>
-                    <div class="card-produto-img">
-                        <img src="<?= BASE_URL ?>/assets/uploads/products/<?= htmlspecialchars($product['image']) ?>" class="card-img-top">
+                    <div class="product-detail__img-wrapper">
+                        <img src="<?= BASE_URL ?>/assets/uploads/products/<?= htmlspecialchars($product['image']) ?>" class="card-img-top product-detail__img">
                     </div>
                 <?php else: ?>
-                    <p class="card-text">
-                        <?= $text['no_image_set_to_this_product'] ?>
-                    </p>
+                    <div class="product-detail__img-placeholder">
+                        <p class="card-text">
+                            <?= $text['no_image_set_to_this_product'] ?>
+                        </p>
+                    </div>
                 <?php endif; ?>
 
-                <div class="card-body card-produto-body">
-                    <h5 class="card-title card-produto-titulo">
+                <div class="card-body product-detail__body">
+                    <h5 class="card-title product-detail__name">
                         <?= htmlspecialchars($product['name']) ?>
                     </h5>
-                    <p class="card-text card-produto-preco">
+                    <p class="card-text product-detail__price">
                         R$ <?= htmlspecialchars($product['price']) ?>
                     </p>
-                    <p class="card-text">
+                    <p class="card-text product-detail__description">
                         <?= htmlspecialchars($product['description']) ?>
                     </p>
-                    <p class="card-text fw-bold">
-                        <?= $text['dimensions'] ?>
-                    </p>
-                    <p class="card-text">
-                        <?= $text['height'] ?> <?= $product['height'] ?> cm
-                    </p>
-                    <p class="card-text">
-                        <?= $text['width'] ?> <?= $product['width'] ?> cm
-                    </p>
-                    <p class="card-text">
-                        <?= $text['weight'] ?> <?= $product['weight'] ?> kg
-                    </p>
-
-                    <div class="container card-produto-botao text-center">
+                    <div class="product-detail__dimensions">
+                        <p class="card-text fw-bold">
+                            <?= $text['dimensions'] ?>
+                        </p>
+                        <p class="card-text product-detail__dimension">
+                            <?= $text['height'] ?> <?= $product['height'] ?> cm
+                        </p>
+                        <p class="card-text product-detail__dimension">
+                            <?= $text['width'] ?> <?= $product['width'] ?> cm
+                        </p>
+                        <p class="card-text product-detail__dimension">
+                            <?= $text['weight'] ?> <?= $product['weight'] ?> kg
+                        </p>
+                    </div>
+                    <div class="container product-detail__action text-center">
                         <a href="cart.php?add=<?= $product['id'] ?>" class="btn btn-primary w-100 text-nowrap">
                             <?= $text['add_to_cart'] ?>
                         </a>
