c44729c4f6c1242dec2d7d711974da930b17dfbf
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-09-23 14:53:55 -0300

    Adiciona tratamento de erro na criação de usuários
    A estrutura try e catch é utilizada para tratamento de erro caso dois usuários tenham o mesmo nome de usuário (username)

--- admin/users/store.php
+++ admin/users/store.php
@@ -3,22 +3,38 @@
 require_once '../../config/database.php';
 require_once '../../config/app.php';
 
+require_once __DIR__ . '/../../includes/header.php';
+require_once __DIR__ . '/../../includes/navbar.php';
+
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 $role = $_POST['role'] ?? '';
 
-
-$sql = "INSERT INTO users (username, password, role)
-        Values (:username, :password, :role)";
-
-$stmt = $conn->prepare($sql);
-
-$password_hash = password_hash($password, PASSWORD_DEFAULT);
-
-$stmt->execute([
-    ':username' => $username,
-    ':password' => $password_hash,
-    ':role' => $role
-])
-
+try{
+    $sql = "INSERT INTO users (username, password, role)
+            Values (:username, :password, :role)";
+    
+    $stmt = $conn->prepare($sql);
+    
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    
+    $stmt->execute([
+        ':username' => $username,
+        ':password' => $password_hash,
+        ':role' => $role
+    ]);
+} catch (PDOException $e){
+    $errorMessage = $text['error_signup'];
+ }
 ?>
+
+<?php if (!empty($errorMessage)): ?>
+    <div class="alert alert-danger">
+        <p><?= htmlspecialchars($errorMessage) ?></p>
+    </div>
+<?php else: ?>
+    <?php 
+    header('location: index.php');
+    exit; 
+    ?>
+<?php endif; ?>
